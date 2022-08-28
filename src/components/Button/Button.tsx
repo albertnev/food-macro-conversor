16bbbfa09605134e3af16af2b853137a268d133c
= <=>
2022-08-28 15:03:18 +0200

    Add basic control components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import cx from 'classnames';
 import styles from './Button.module.scss';
 
 interface ButtonProps {
   label: string;
   onClick: () => void;
+  small?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => (
-  <button className={styles.button} type="button" onClick={onClick}>
+const Button: React.FC<ButtonProps> = ({ label, onClick, small }) => (
+  <button
+    className={cx({ [styles.button]: true, [styles['button--small']]: small })}
+    type="button"
+    onClick={onClick}
+  >
     {label}
   </button>
 );
+
+Button.defaultProps = {
+  small: false,
+};
 
 export default Button;
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import cx from 'classnames';
+import debounce from 'lodash.debounce';
+
 import styles from './Input.module.scss';
 
 interface InputProps {
+  defaultValue?: string;
+  icon?: React.ReactNode;
   onChange: (val: string) => void;
+  placeholder?: string;
 }
 
-const Input: React.FC<InputProps> = ({ onChange }) => (
-  <input
-    className={styles.input}
-    type="text"
-    onChange={({ target: { value } }) => onChange(value)}
-  />
-);
+const Input: React.FC<InputProps> = ({
+  defaultValue,
+  icon,
+  onChange,
+  placeholder,
+}) => {
+  const debouncedOnChange = useMemo(
+    () =>
+      debounce((val: string) => {
+        onChange(val);
+      }, 500),
+    [onChange],
+  );
+
+  // Cancel the debounced call if unmounted
+  useEffect(
+    () => () => {
+      debouncedOnChange.cancel();
+    },
+    [debouncedOnChange],
+  );
+
+  return (
+    <div className={cx('input', styles.inputContainer)}>
+      {icon && <div className={styles.icon}>{icon}</div>}
+      <input
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        type="text"
+        onChange={({ target: { value } }) => debouncedOnChange(value)}
+      />
+    </div>
+  );
+};
+
+Input.defaultProps = {
+  defaultValue: '',
+  icon: undefined,
+  placeholder: '',
+};
 
 export default Input;
