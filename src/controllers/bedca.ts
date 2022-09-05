8e7fa9ee0b4398e80f43bc2804efd7d0841fc48a
= <=>
2022-09-05 12:09:24 +0200

    Switch from using CURL to use fetch

--- src/controllers/bedca.ts
+++ src/controllers/bedca.ts
@@ -1,4 +1,3 @@
-import { exec } from 'child_process';
 import parser from 'xml2json';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { readFileSync } from 'fs';
@@ -21,33 +20,18 @@ const searchFood = async (searchText: string) => {
     .replace('{{searchTerm}}', searchText)
     .replace(/(\r\n|\n|\r)/gm, '');
 
-  const args = [
-    '-X POST',
-    'https://www.bedca.net/bdpub/procquery.php',
-    `-d "${xmlQuery.replaceAll('"', '\\"')}"`,
-    '-H "Content-Type: text/xml; charset=UTF-8"',
-    '-H "Origin: https://www.bedca.net"',
-    '-H "Accept: application/xml"',
-  ];
-
-  return new Promise((resolve, reject) => {
-    exec(`curl ${args.join(' ')}`, (error: any, stdout: any, stderr: any) => {
-      // eslint-disable-next-line no-console
-      if (stderr || error) console.log('BEDCA error:', stdout, stderr, error);
-
-      if (stdout) {
-        try {
-          const obj = parser.toJson(stdout, { object: true });
-          const normalizedResponse = normalizeSearchResponse(obj);
-          resolve(normalizedResponse);
-        } catch (err) {
-          reject(err);
-        }
-      } else {
-        reject(stderr);
-      }
-    });
+  const response = await fetch('https://www.bedca.net/bdpub/procquery.php', {
+    body: xmlQuery,
+    headers: {
+      Accept: 'application/xml',
+      'Content-Type': 'text/xml; charset=UTF-8',
+    },
+    method: 'POST',
   });
+
+  const responseText = await response.text();
+  const parsedData = parser.toJson(responseText, { object: true });
+  return normalizeSearchResponse(parsedData);
 };
 
 /* API METHODS */
@@ -64,27 +48,22 @@ const apiGetFoodDetails = async (
     .replace('{{foodId}}', req.query.id as string)
     .replace(/(\r\n|\n|\r)/gm, '');
 
-  const args = [
-    '-X POST',
-    'https://www.bedca.net/bdpub/procquery.php',
-    `-d "${xmlQuery.replaceAll('"', '\\"')}"`,
-    '-H "Content-Type: text/xml; charset=UTF-8"',
-    '-H "Origin: https://www.bedca.net"',
-    '-H "Accept: application/xml"',
-  ];
+  try {
+    const response = await fetch('https://www.bedca.net/bdpub/procquery.php', {
+      body: xmlQuery,
+      headers: {
+        Accept: 'application/xml',
+        'Content-Type': 'text/xml; charset=UTF-8',
+      },
+      method: 'POST',
+    });
 
-  exec(`curl ${args.join(' ')}`, (error: any, stdout: any, stderr: any) => {
-    if (stdout) {
-      try {
-        const obj = parser.toJson(stdout, { object: true });
-        res.status(200).json(normalizeFoodDetailsResponse(obj));
-      } catch (err) {
-        res.status(400).send(stdout);
-      }
-    } else {
-      res.status(502).send(stderr);
-    }
-  });
+    const responseText = await response.text();
+    const parsedData = parser.toJson(responseText, { object: true });
+    res.status(200).json(normalizeFoodDetailsResponse(parsedData));
+  } catch (err) {
+    res.status(425).send(err);
+  }
 };
 
 const apiSearchFood = async (
