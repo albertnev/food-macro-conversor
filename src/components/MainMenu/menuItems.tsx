2c5fead01dfa7a0883b826815b3205c9f152029f
= <=>
2022-09-29 00:55:53 +0200

    Restore nex-auth functionality

--- src/components/MainMenu/menuItems.tsx
+++ src/components/MainMenu/menuItems.tsx
@@ -1,4 +1,4 @@
-// import { signIn, signOut } from 'next-auth/react';
+import { signIn, signOut } from 'next-auth/react';
 import { IoMdLogIn, IoMdLogOut } from 'react-icons/io';
 import { FaBalanceScale } from 'react-icons/fa';
 import { BiGitCompare } from 'react-icons/bi';
@@ -49,15 +49,15 @@ export const menuItems = [
     url: navigation.about,
   },
   {
-    accessLevel: 'signed',
-    icon: <IoMdLogOut data-testid="menu-icon-logout" />,
-    label: 'logout',
-    onClick: () => null,
-  },
-  {
     accessLevel: 'unsigned',
     icon: <IoMdLogIn data-testid="menu-icon-login" />,
     label: 'login',
-    onClick: () => null,
+    onClick: signIn,
+  },
+  {
+    accessLevel: 'signed',
+    icon: <IoMdLogOut data-testid="menu-icon-logout" />,
+    label: 'logout',
+    onClick: signOut,
   },
 ];
